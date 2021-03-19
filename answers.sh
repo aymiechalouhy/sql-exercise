0eb69f7504943f9e8795ac0ccfa2b827321ca590
Aymie <aymie.walid.chalouhy@gmail.com>
2021-03-20 00:32:15 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,26 @@
+1) select * from students
+
+
+2) SELECT * FROM students
+   WHERE Age > 30;
+
+
+3)  SELECT * FROM students
+    WHERE Gender="F" 
+    And age=30;
+
+
+4)  SELECT Points FROM students
+    WHERE Name="Alex" 
+
+
+5) insert into students values (8,'Aymie',25,'F',100);
+
+
+6)  UPDATE Students 
+    SET points = points + 50
+    WHERE Name='Basma';
+
+7) UPDATE Students 
+   SET points = points - 10
+   WHERE Name='alex';
